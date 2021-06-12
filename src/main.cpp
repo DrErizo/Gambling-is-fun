1c214e41f9c4973934e283e87ca6fc17d23da64c
Erizo <WhiteShadow4007@gmail.com>
2021-06-12 22:40:50 +0200

    another small fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,16 @@
 #include <windows.h>
 #include <conio.h>
 int main(int argc, char** args) {
-    srand(time(NULL));
     std::string mode; 
     uint16_t choice, num1, num2, num3, roll,roulette;
     uint64_t bet,ballance = 100; 
     bool running;
-    num1 = rand() % 10 + 1;
-    num2 = rand() % 10 + 1;
-    num3 = rand() % 10 + 1;
-    roll = rand() % 6 + 1;
-    roulette = rand() % 8 + 1;
     
     std::cout << "welcome to the casino, we gave you a starting sum of 100$ your goal is to reach 100 000 000$\nNow the choice of how you spend it belonges to you\n\n";
    
     
     while(running){
+    srand(time(NULL));
     std::cout << "1-The one handed thief\nif all the numbers are 7 the the amount of money you put into it will be multiplied by 777 but be carefull if the numbes are any different you lose everything you bet\n\n"
                  "2-dice rolling\nif you want to win you need to roll 6 or 5 in any other case you'll lose\non win your winnings will be multiplied by 5\n\n"
                  "3-russian roulette\na very dangerous game but a really rewarding one too if you win your bet will be multiplied by a 1000 but if you lose......all your ballance will be wiped out\n\n"
@@ -36,6 +31,9 @@ int main(int argc, char** args) {
         
         std::cout << "you chose " << mode << "\n\n";
         case 1:
+            num1 = rand() % 9 + 1;
+            num2 = rand() % 9 + 1;
+            num3 = rand() % 9 + 1;
             
             mode = "the one handed thief";
             Sleep(500);
@@ -45,7 +43,7 @@ int main(int argc, char** args) {
             Sleep(500);
             std::cout <<num3 << "\n\n";
             Sleep(500);
-            if(num1 && num2 && num3 == 7){
+            if(num1 + num2 + num3 == 21){
                 std::cout << "congratulations!!!\nyou won:" << bet * 777<< "$";
                 ballance += bet * 777;
             }
@@ -56,6 +54,7 @@ int main(int argc, char** args) {
             break;
         case 2:
             
+           roll = rand() % 5 + 1;
            mode = "dice rolling";
             Sleep(500);
             if(roll == 5,6){
@@ -63,13 +62,14 @@ int main(int argc, char** args) {
                 std::cout<< "what a lucky roll, perhaps you should try again and continue this streak?\n you win:"<<bet * 5<< "$"; 
                 ballance += bet * 5;
             }
-            else if (roll == 0,1,2,3,4){
+            else {
                 std::cout<< "it hurts my soul to see that maybe next round will be different\nyou lose:" << bet<< "$";
                 ballance -= bet;
             }
             break;
 
         case 3:
+            roulette = rand() % 8 + 1;
             
             mode = "russian roulette";
             Sleep(500);
@@ -77,9 +77,8 @@ int main(int argc, char** args) {
                 std::cout << "phew...that one was intense but congratulations\nyou win" << bet * 500 << "$\n";
                 ballance += bet * 1000;
             }
-            else if(roulette == 0,2,3,4,5,6,7,8){
+            else{
                 std::cout << "*You Die*";
-                Slepp(1000);
                 std::cout << "......that's life well not anymore\n more money for me I guess";
                 exit(0);
             }
@@ -100,7 +99,7 @@ int main(int argc, char** args) {
             running = true; 
         }
         else{
-            exit(1);
+            exit(0);
         }
     }
     return 0;
